f2946ad5491fb0e1f15299c03411508db43764ad
Wouter Altepost <113669277+WouterAltepost@users.noreply.github.com>
2025-06-08 16:08:20 +0200

    Update HighLife.cs

--- Conway-student/HighLife.cs
+++ Conway-student/HighLife.cs
@@ -1,23 +1,23 @@
-﻿//using System.Windows;
+﻿using System.Windows;
 
-//namespace ConwayGameOfLife
-//{
- //   public class HighLife : ConwayGameOfLife
- //   {
-//        protected override bool CellShouldLive(bool livingCell, int neighbourCount)
-//       {
+namespace ConwayGameOfLife
+{
+    public class HighLife : ConwayGameOfLife
+    {
+        protected override bool CellShouldLive(bool livingCell, int neighbourCount)
+       {
             // B36/S23
 
             // a cell is born if it has 3 or 6 neighbors
-//            if (!livingCell && (neighbourCount == 3 || neighbourCount == 6))
-//                return true;
+            if (!livingCell && (neighbourCount == 3 || neighbourCount == 6))
+                return true;
 
             // a cell survives if it has 2 or 3 neighbors
-//            if (livingCell && (neighbourCount == 2 || neighbourCount == 3))
-//                return true;
+            if (livingCell && (neighbourCount == 2 || neighbourCount == 3))
+                return true;
 
             // dead cell
-//            return false;
-//        }
-//  }
-//}
+            return false;
+        }
+  }
+}
